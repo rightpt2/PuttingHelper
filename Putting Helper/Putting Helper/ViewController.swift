1ec87daf9c207cb5719a9e27b9868b918781890a
Ben <ben@Bennys-Python-Machine.local>
2018-08-29 14:39:34 -0700

    First commit

---
+++ Putting Helper/Putting Helper/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Putting Helper
+//
+//  Created by Ben on 8/28/18.
+//  Copyright © 2018 Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var PuttSkill: UITextField!
+    
+    @IBOutlet var Length: UITextField!
+    
+    @IBOutlet var RiseFall: UITextField!
+   
+    @IBOutlet var Slope: UITextField!
+    
+    @IBOutlet var PuttCalc: UIButton!
+   
+    @IBOutlet var OutputPutts: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
